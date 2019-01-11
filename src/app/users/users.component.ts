ff06146e5c8cb3263a4bb5724ce584af2d6783bb
Kishan Patel <kolipatelkishan786@gmail.com>
2019-01-11 20:27:42 +0530

    120 Passing Parameters to Routes.

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -6,6 +6,20 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./users.component.scss']
 })
 export class UsersComponent implements OnInit {
+  users = [
+    {
+      id: 1,
+      name: 'Max'
+    },
+    {
+      id: 2,
+      name: 'Anna'
+    },
+    {
+      id: 3,
+      name: 'Chris'
+    }
+  ];
 
   constructor() { }
 
