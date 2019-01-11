f65894d82856d96f0215f7bf865e08394b5f3e0e
Kishan Patel <kolipatelkishan786@gmail.com>
2019-01-11 17:19:39 +0530

    Revert "Add components and bootstrap."
    This reverts commit d549512f5ade72b6c81134e62a25ffdba6add93f.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,22 +3,10 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './home/home.component';
-import { ServersComponent } from './servers/servers.component';
-import { EditServerComponent } from './servers/edit-server/edit-server.component';
-import { ServerComponent } from './servers/server/server.component';
-import { UsersComponent } from './users/users.component';
-import { UserComponent } from './users/user/user.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    HomeComponent,
-    ServersComponent,
-    EditServerComponent,
-    ServerComponent,
-    UsersComponent,
-    UserComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
