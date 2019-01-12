aa6d24f7f0a5a43b1b431c656271e2cbc5a7cdf8
Kishan Patel <kolipatelkishan786@gmail.com>
2019-01-12 11:01:01 +0530

    123 An Important Note about Route Observables.

--- src/app/users/user/user.component.ts
+++ src/app/users/user/user.component.ts
@@ -1,13 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute, Params} from '@angular/router';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.scss']
 })
-export class UserComponent implements OnInit {
+export class UserComponent implements OnInit, OnDestroy {
   user: {id: number, name: string};
+  paramsSubsciption: Subscription;
+
   constructor(private route: ActivatedRoute) { }
 
   ngOnInit() {
@@ -15,13 +18,16 @@ export class UserComponent implements OnInit {
       id: this.route.snapshot.params['id'],
       name: this.route.snapshot.params['name']
     };
-    this.route.params
+    this.paramsSubsciption = this.route.params
       .subscribe(
         (params: Params) => {
           this.user.id = params['id'];
           this.user.name = params['name'];
         }
       );
+  }
+  ngOnDestroy() {
+    this.paramsSubsciption.unsubscribe();
   }
 
 }
