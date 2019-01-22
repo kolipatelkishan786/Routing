ee96bcad247b1ff828ab0f56a917a7182a82e793
Kishan Patel <kolipatelkishan786@gmail.com>
2019-01-22 17:29:40 +0530

    129 Configuring the Handling of Query Parameters.

--- src/app/servers/server/server.component.ts
+++ src/app/servers/server/server.component.ts
@@ -26,7 +26,7 @@ export class ServerComponent implements OnInit {
       );
   }
   onEdit() {
-    this.router.navigate(['edit'], {relativeTo: this.route});
+    this.router.navigate(['edit'], {relativeTo: this.route, queryParamsHandling: 'preserve'});
   }
 
 }
